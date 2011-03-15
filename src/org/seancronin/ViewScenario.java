84de45eb32a0907b8ca3e52899284945a7b269c6
Sean Cronin <seancron@gmail.com>
2011-03-14 21:45:35 -0400

    Menu suggestion tip and code cleanup
    * Added text to alert people to the suggest a scenario menu
    * Deleted extraneous code

--- src/org/seancronin/ViewScenario.java
+++ src/org/seancronin/ViewScenario.java
@@ -1,7 +1,5 @@
 package org.seancronin;
 
-import java.util.Random;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
@@ -12,16 +10,12 @@ import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.GestureDetector.SimpleOnGestureListener;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class ViewScenario extends Activity {
     private final int MENU_SUGGESTION = 1;
     private final int GROUP_DEFAULT = 0;
     private GestureDetector gestureDetector;
     TextView scenarioText;
-    final static Random scenarioNumber = new Random();
-    private int[] scenarioOrder;
-    private int shufflePosition = 0;
     private ScenarioGenerator scenarioGenerator;
 
     /** Called when the activity is first created. */
@@ -30,18 +24,7 @@ public class ViewScenario extends Activity {
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.main2);
 
-	//Resources res = getResources();
-	
 	scenarioGenerator = new ScenarioGenerator(getApplicationContext());
-
-	/*
-	scenarioOrder = new int[res.getStringArray(R.array.scenarios).length];
-	for (int i = 0; i < scenarioOrder.length; i++) {
-	    scenarioOrder[i] = i;
-	}
-
-	shuffle(scenarioOrder);
-	*/
 
 	gestureDetector = new GestureDetector(this,
 		new SimpleOnGestureListener() {
@@ -51,7 +34,7 @@ public class ViewScenario extends Activity {
 			return true;
 		    }
 		});
-	
+
 	randomScenario();
     }
 
@@ -60,9 +43,6 @@ public class ViewScenario extends Activity {
 	super.onSaveInstanceState(outState);
 
 	scenarioText = (TextView) findViewById(R.id.ScenarioBody);
-
-	outState.putIntArray("scenarioOrder", scenarioOrder);
-	outState.putInt("shufflePosition", shufflePosition);
 	outState.putCharSequence("scenarioText", scenarioText.getText());
     }
 
@@ -76,9 +56,6 @@ public class ViewScenario extends Activity {
 	super.onRestoreInstanceState(savedInstanceState);
 
 	scenarioText = (TextView) findViewById(R.id.ScenarioBody);
-
-	scenarioOrder = savedInstanceState.getIntArray("scenarioOrder");
-	shufflePosition = savedInstanceState.getInt("shufflePosition");
 	scenarioText
 		.setText(savedInstanceState.getCharSequence("scenarioText"));
     }
@@ -107,38 +84,10 @@ public class ViewScenario extends Activity {
 	return super.onOptionsItemSelected(item);
     }
 
-    public void createAlert() {
-	Toast.makeText(this, "Next scenario", Toast.LENGTH_SHORT).show();
-    }
-
-    /**
-     * 
-     * @param array
-     * @see <a
-     *      href="http://en.wikipedia.org/w/index.php?title=Fisher-Yates_shuffle">Fisher-Yates
-     *      Shuffle (Wikipedia)</a>
-     */
-    public static void shuffle(int[] array) {
-	for (int i = array.length; i > 1; i--) {
-	    int j = scenarioNumber.nextInt(i);
-	    int tmp = array[j];
-	    array[j] = array[i - 1];
-	    array[i - 1] = tmp;
-	}
-    }
-
     public void randomScenario() {
-	//Resources res = getResources();
-	//String[] scenarios = res.getStringArray(R.array.scenarios);
 	String scenario = scenarioGenerator.generateScenario();
 
 	scenarioText = (TextView) findViewById(R.id.ScenarioBody);
 	scenarioText.setText(scenario);
-
-	/* shufflePosition++;
-	if (shufflePosition >= scenarios.length) {
-	    shuffle(scenarioOrder);
-	    shufflePosition = 0;
-	} */
     }
 }
